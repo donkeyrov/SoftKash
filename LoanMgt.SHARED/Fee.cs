f71d5eb83a324a9cc674343df7a58be4207af457
Mumba K Mumba <kayebele@yahoo.com>
2022-04-03 22:03:32 +0200

    update azure settings in application

--- LoanMgt.SHARED/Fee.cs
+++ LoanMgt.SHARED/Fee.cs
@@ -24,8 +24,11 @@ namespace LoanMgt.SHARED
         public bool DeductFromPrincipalAmount { get; set; }
         public bool Capitalized { get; set; }
         public bool Monthly { get; set; }
+        public int TransactionCodeId { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
+
+
 
     }
 }
---
+++ LoanMgt.UI/Data/Migrations/20220324233335_added tran code id to fee entity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoanMgt.UI.Data.Migrations
+{
+    public partial class addedtrancodeidtofeeentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionCodeId",
+                table: "Fees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionCodeId",
+                table: "Fees");
+        }
+    }
+}
