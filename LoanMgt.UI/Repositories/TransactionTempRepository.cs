abeb94c84662b3274792e4674de2fe38f1c21417
Mumba K Mumba <kayebele@yahoo.com>
2022-04-17 14:54:11 +0200

    added repository for transactionTemp entity

---
+++ LoanMgt.UI/Repositories/TransactionTempRepository.cs
@@ -0,0 +1,13 @@
+﻿using LoanMgt.SHARED;
+using LoanMgt.UI.Data;
+
+namespace LoanMgt.UI.Repositories
+{
+    public class TransactionTempRepository:GenericRepository<TransactionTemp>,ITransactionTempRepository
+    {
+        public TransactionTempRepository(ApplicationDbContext db) : base(db)
+        {
+
+        }
+    }
+}
