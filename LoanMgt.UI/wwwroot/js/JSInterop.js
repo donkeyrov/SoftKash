cdff22cc57b2a87432c8d202668fa747f181af4e
Mumba K Mumba <kayebele@yahoo.com>
2022-05-01 17:30:17 +0200

    fixed navbar menu height issue on long pages

---
+++ LoanMgt.UI/wwwroot/js/JSInterop.js
@@ -0,0 +1,14 @@
+﻿function AddDataTable(table, searching, firstTime) {
+    
+      tt =   $(table).DataTable({
+          "searching": searching,
+          retrieve: true,
+            dom: 'Bfrtip',
+            buttons: [
+                'excel', 'pdf'
+            ]
+        });
+   
+    
+
+}
