0bb298fd69b337f6ce46072bba6e6e82945f6ad9
donkeyrov <kayebele@yahoo.com>
2022-06-06 17:00:06 +0200

    fixed loan submission IU

---
+++ LoanMgt.UI/Data/Migrations/20220606145749_sorrected spelling of suspended on loan entity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoanMgt.UI.Data.Migrations
+{
+    public partial class sorrectedspellingofsuspendedonloanentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Suspened",
+                table: "Loans",
+                newName: "Suspended");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Suspended",
+                table: "Loans",
+                newName: "Suspened");
+        }
+    }
+}
