bd327a4d4c7d8d3e95f565ad39a08b105c8217a3
donkeyrov <kayebele@yahoo.com>
2022-06-06 19:32:37 +0200

    Fixed exports on loan submission UI

---
+++ LoanMgt.SHARED/TempLoan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanMgt.SHARED
+{
+    [NotMapped]
+    public class TempLoan
+    {
+        public bool Selected { get; set; }
+        public string EmployeeNo { get; set; }
+        public string OldWageType { get; set; }
+        public string NewWageType { get; set; }
+        public string EndDate { get; set; }
+        public string StartDate { get; set; }
+        public string Instalment { get; set; }
+        public string Cheque { get; set; }
+        public string ZLSCH { get; set; }
+        public string Nrc { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+    }
+}
